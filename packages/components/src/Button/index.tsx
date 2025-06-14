e86cddb9868b5bd26f957b7dcd1ad62ad178de57
22ha <zhang2ha@qq.com>
2025-06-14 23:22:12 +0800

    changesets test

---
+++ packages/components/src/Button/index.tsx
@@ -0,0 +1,32 @@
+import { CSSProperties, MouseEventHandler, PropsWithChildren } from 'react'
+import Styles from './index.module.scss'
+import cs from 'classnames'
+
+export interface ButtonProps extends PropsWithChildren {
+  className?: string
+  style?: CSSProperties
+  type?: 'primary' | 'default'
+  onClick?: MouseEventHandler
+}
+
+function Button(props: ButtonProps) {
+  const {
+    className = '',
+    style,
+    type = 'primary',
+    children,
+    onClick = () => {},
+  } = props
+
+  return (
+    <div
+      className={cs(Styles.btn, className, Styles[`btn-${type}`])}
+      style={{ ...style }}
+      onClick={onClick}
+    >
+      {children}
+    </div>
+  )
+}
+
+export default Button

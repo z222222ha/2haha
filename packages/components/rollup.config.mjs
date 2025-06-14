51c8b94c783cb565e1c5b5097fc6185312fb0082
22ha <zhang2ha@qq.com>
2025-06-14 23:51:46 +0800

    action test

---
+++ packages/components/rollup.config.mjs
@@ -0,0 +1,45 @@
+import postcss from 'rollup-plugin-postcss'
+import typescript from '@rollup/plugin-typescript'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import replace from '@rollup/plugin-replace'
+
+/** @type {import("rollup").RollupOptions} */
+export default {
+  input: 'src/index.ts',
+  external: ['react', 'react-dom'],
+  output: [
+    {
+      file: 'dist/esm.js',
+      format: 'esm',
+    },
+    {
+      file: 'dist/cjs.js',
+      format: 'cjs',
+    },
+    {
+      file: 'dist/umd.js',
+      name: 'Haha',
+      format: 'umd',
+      globals: {
+        react: 'React',
+        'react-dom': 'ReactDOM',
+      },
+    },
+  ],
+  plugins: [
+    resolve(),
+    commonjs(),
+    typescript({
+      tsconfig: 'tsconfig.json',
+    }),
+    postcss({
+      modules: true,
+      extract: false,
+      use: ['sass'],
+    }),
+    replace({
+      'process.env.NODE_ENV': '"production"',
+    }),
+  ],
+}

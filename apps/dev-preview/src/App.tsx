5acd492a4c5177cb2c3163b8653d7ced60513206
ZHH <zhang2ha@qq.com>
2025-06-17 11:34:10 +0800

    workflow test

--- apps/dev-preview/src/App.tsx
+++ apps/dev-preview/src/App.tsx
@@ -34,9 +34,6 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
       <Button
         type="primary"

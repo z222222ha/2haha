e1d6c8ef0e52ebcca90ddfcb4f5e4d8be4242c5f
ZHH <zhang2ha@qq.com>
2025-06-18 15:01:45 +0800

    add home page

---
+++ apps/home/app/playground/page.tsx
@@ -0,0 +1,32 @@
+'use client'
+import React, { useState, useEffect } from 'react'
+
+export default function Playground() {
+  // 进入该页面后，将滚动条向下滚动一个 header 高度的距离
+  useEffect(() => {
+    const header = document.querySelector('header')
+    if (header) {
+      window.scrollTo(0, header.offsetHeight)
+    }
+  }, [])
+
+  return (
+    <iframe
+      title="playground"
+      id="pg"
+      src="http://playground.djbgem.xyz"
+      sandbox="allow-scripts allow-same-origin"
+      loading="lazy"
+      allowFullScreen
+      aria-hidden="false"
+      tabIndex={0}
+      style={{
+        width: '100%',
+        height: `100vh`,
+        border: 0,
+        position: 'absolute',
+        left: 0,
+      }}
+    />
+  )
+}

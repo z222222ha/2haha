1fe406d2f228709e19f9cc2b142f796d020f90b8
ZHH <zhang2ha@qq.com>
2025-06-18 11:15:39 +0800

    update playground

--- apps/playground/src/template/App.tsx
+++ apps/playground/src/template/App.tsx
@@ -1,22 +1,47 @@
 import { useState } from 'react'
 import './App.css'
-import { Button } from '@2haha/components'
+import { Button, Calendar } from '@2haha/components'
+import { useHover } from '@2haha/hooks'
+import dayjs from 'dayjs'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  // useHover
+  const element = (hovered: boolean) => <div>Hover me! {hovered && 'Thanks'}</div>
+
+  const [hoverable, hovered] = useHover(element)
+
   return (
     <>
-      <h1>Hello World</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-      </div>
+      <h1>Hello World {count}</h1>
       <Button
         type="default"
-        onClick={() => alert(1)}
+        onClick={() => setCount((count) => count + 1)}
       >
-        按钮二
+        +1
       </Button>
+
+      <h1>useHover</h1>
+      <div style={{ height: 100 }}>
+        {hoverable}
+        <div>{hovered ? 'HOVERED' : ''}</div>
+      </div>
+
+      <h1>Calendar</h1>
+      <Calendar
+        value={dayjs(new Date())}
+        // renderCell={(date) => {
+        //   return <div style={{ background: "yellowgreen" }}>{date.format("YYYY-MM-DD")}</div>;
+        // }}
+        // renderCellContent={(date) => {
+        //   return <div style={{ background: 'yellowgreen' }}>{date.format('YYYY-MM-DD')}</div>
+        // }}
+        locale="en-US"
+        onChange={(date) => {
+          console.log('click:', date.format('YYYY-MM-DD'))
+        }}
+      />
     </>
   )
 }
